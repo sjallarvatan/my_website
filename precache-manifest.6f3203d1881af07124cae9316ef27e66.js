0b58cc3e5e1c061d87b2d377cb62c74002de7f1c
Marcus Walker <marcuswalker@Marcuss-MBP.domain.name>
2019-10-13 21:07:32 +0100

    Updates

---
+++ precache-manifest.6f3203d1881af07124cae9316ef27e66.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430ea1d5d464d0baa7abfc34d7f0a83d",
+    "url": "/sjallarvatan/my_website.git/index.html"
+  },
+  {
+    "revision": "fe740ad25b110283c52f",
+    "url": "/sjallarvatan/my_website.git/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "6eba1af9e0806d52bfbd",
+    "url": "/sjallarvatan/my_website.git/static/css/main.eb6f88fe.chunk.css"
+  },
+  {
+    "revision": "fe740ad25b110283c52f",
+    "url": "/sjallarvatan/my_website.git/static/js/2.f9ef4261.chunk.js"
+  },
+  {
+    "revision": "6eba1af9e0806d52bfbd",
+    "url": "/sjallarvatan/my_website.git/static/js/main.49635dee.chunk.js"
+  },
+  {
+    "revision": "a9c5812e990f2e7200c2",
+    "url": "/sjallarvatan/my_website.git/static/js/runtime~main.e31c4c57.js"
+  },
+  {
+    "revision": "2a6a0a5a046ed5b18c0015c5776957fa",
+    "url": "/sjallarvatan/my_website.git/static/media/ICL_red.2a6a0a5a.png"
+  },
+  {
+    "revision": "b339a0ae87c6c110ff04433308709dc9",
+    "url": "/sjallarvatan/my_website.git/static/media/LI_tube.b339a0ae.jpg"
+  },
+  {
+    "revision": "06daf86e3d51ddf23a29020c6a258ba7",
+    "url": "/sjallarvatan/my_website.git/static/media/mel_wet.06daf86e.jpg"
+  }
+]);
